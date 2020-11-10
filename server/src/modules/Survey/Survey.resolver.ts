5cc765a490025a341cd609b7a66f98c6580323c0
Pradosh Sinha <pradoshks@outlook.com>
2020-11-10 16:08:24 +1100

    make queries nullable

--- server/src/modules/Survey/Survey.resolver.ts
+++ server/src/modules/Survey/Survey.resolver.ts
@@ -4,22 +4,24 @@ import { Survey } from "../../entity/Survey.entity";
 @Resolver()
 export class SurveyResolver {
   // TODO  Another query when loading only a specific survey then shouldn't be an array.
-  @Query(() => [Survey])
+  @Query(() => [Survey], { nullable: true })
   async surveys(@Arg("id", { nullable: true }) id?: string) {
     if (id) {
-      return Survey.find({
-        id: parseInt(id),
-      });
+      return (
+        Survey.find({
+          id: parseInt(id),
+        }) ?? null
+      );
     } else {
-      return Survey.find();
+      return Survey.find() ?? null;
     }
   }
 
-  @Query(() => Survey)
-  async survey(@Arg("id", { nullable: true }) id: string) {
+  @Query(() => Survey, { nullable: true })
+  async survey(@Arg("id", { nullable: false }) id: string) {
     const survey = await Survey.findOne({
       id: parseInt(id),
     });
-    return survey;
+    return survey ?? null;
   }
 }
